5a354c66fbe72d5eecdd88fd0daa9ed83188d35e
vbbphc <balloli.vb@gmail.com>
2019-10-10 23:54:10 +0530

    Adds: MACs usng forward hooks

--- src/test.py
+++ src/test.py
@@ -5,18 +5,18 @@ from utils import *
 from thop import profile
 
 model = resnet18()
-model.eval()
-model.apply(add_hook)
-with torch.no_grad():
-    model(torch.ones(1, 3, 224, 224))
-total_ai = 0
-for m in model.modules():
-    if len(list(m.children())) > 0:  # skip for non-leaf module
-        continue
-    total_ai += m.total_ai
-print(total_ai.item())
+# model.eval()
+# model.apply(add_hook)
+# with torch.no_grad():
+#     model(torch.ones(1, 3, 224, 224))
+# total_ai = 0
+# for m in model.modules():
+#     if len(list(m.children())) > 0:  # skip for non-leaf module
+#         continue
+#     total_ai += m.total_ai
+# print(total_ai.item())
 
 # for c in model.children():
 #     mo
-#ai = ArithmeticIntensity(model=model, input_dims=(1, 3, 224, 224))
-#ai.get_metrics()
+ai = ArithmeticIntensity(model=model, input_dims=(1, 3, 224, 224))
+print(ai.get_metrics())
